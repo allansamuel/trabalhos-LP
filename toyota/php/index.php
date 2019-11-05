2028be6bf40670f0dbb6a5d4fbd1598d6fe48909
allansamuel <allangomeseu@hotmail.com>
2019-11-05 09:44:32 -0300

    Add Slider

--- toyota/php/index.php
+++ toyota/php/index.php
@@ -6,8 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="../css/navbar.css">
-    <link rel="stylesheet" href="css/image-slider.css">
-
+    <link rel="stylesheet" href="../css/image-slider.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <style>
+    .fa { color: #fff; }
+</style>
     <title>Document</title>
 </head>
 <body>
@@ -25,24 +28,34 @@
         <!-- main image slider container -->
     <div id="slider-main">
 
-    <!-- previous button -->
-    <button id="prev"><i class="fa fa-angle-double-left"></i></button>
-
-    <!-- image container -->
-    <div id="slider"></div>
-
-    <!-- next button -->
-    <button id="next"><i class="fa fa-angle-double-right"></i></button>
-
-    <!-- pagination bullets -->
-    <div id="circles"></div>
+        <button id="prev"><i class="fa fa-angle-double-left"></i></button>
+        <div id="slider"></div>
+        <button id="next"><i class="fa fa-angle-double-right"></i></button>
+        <div id="circles"></div>
 
     </div>
     <!-- end of main image slider container -->
-</body>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" 
-        integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" 
-        crossorigin="anonymous"></script>
-<script src="js/image-slider.js"></script>
+
+
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
+<!-- image slider script -->
+<script src="../js/image-slider.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</body>
+
 </html>
