9adadce849c20b4418cc44eae599d3f9de564525
allansamuel <allangomeseu@hotmail.com>
2019-11-24 23:32:43 -0300

    Alterando slider

--- toyota/php/index.php
+++ toyota/php/index.php
@@ -8,8 +8,11 @@
     <link rel="stylesheet" href="../css/navbar.css">
     <link rel="stylesheet" href="../css/rodape.css">
     <link rel="stylesheet" href="../css/image-slider.css">
+    <link href="../css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    
     <style>
         body{
             font-family: Lato;
@@ -33,17 +36,71 @@
     </nav>
     <!-- navbar -->
     
-    <!-- slider -->
-    <div id="slider-main">
+    <div style="min-height: 50px;">
+                <!-- Jssor Slider Begin -->
 
-        <button id="prev"><i class="fa fa-angle-double-left"></i></button>
-        <div id="slider"></div>
-        <button id="next"><i class="fa fa-angle-double-right"></i></button>
-        <div id="circles"></div>
+                <style>
+                    /* jssor slider loading skin spin css */
+                    
+                    .jssorl-009-spin img {
+                        animation-name: jssorl-009-spin;
+                        animation-duration: 1.6s;
+                        animation-iteration-count: infinite;
+                        animation-timing-function: linear;
+                    }
+                    
+                    @keyframes jssorl-009-spin {
+                        from {
+                            transform: rotate(0deg);
+                        }
+                        to {
+                            transform: rotate(360deg);
+                        }
+                    }
+                </style>
+                <div id="slider1_container" style="visibility: hidden; position: relative; margin: 0 auto;
+        top: 0px; left: 0px; width: 1300px; height: 250px; overflow: hidden;">
+                    <!-- Loading Screen -->
+                    <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
 
-    </div>
-    <!-- slider -->
+                        <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="../svg/loading/static-svg/spin.svg" />
+                    </div>
 
+                    <!-- Slides Container Begin-->
+                    <div data-u="slides" style="position: absolute; left: 0px; top: 0px; width: 1300px; height: 250px; overflow: hidden; display: flex; align-items: center">
+
+                        <div>
+                            <img data-u="image" src="../img/gallery/banner/005.png" />
+                        </div>
+                        <div>
+                            <img data-u="image" src="../img/gallery/banner/002.jpg" />
+                        </div>
+                        <div>
+                            <img data-u="image" src="../img/gallery/banner/001.jpg" />
+                        </div>
+                    </div>
+                    <!-- Slides Container End-->
+
+                    <!--Logo Begin-->
+                    <div data-u="navigator" class="jssorb031" style="display: flex; flex-direction: row; align-items: center; position: absolute;left: 8%;top: 42%;;color: white;" data-autocenter="1" data-scale="0.5" data-scale-bottom="0.75">
+                        <img src="../img/gallery/icons/logo.png" style="width: 2.5em; ">
+                        <h1 id="title">Vinhos da Serra</h1>
+                    </div>
+                    <!--Logo End-->
+
+                    <!--Bullet Navigator Begin -->
+                    <div data-u="navigator" class="jssorb031" style="position:absolute;bottom:12px;right:12px;" data-autocenter="1" data-scale="0.5" data-scale-bottom="0.75">
+                        <div data-u="prototype" class="i" style="width:16px;height:16px;">
+                            <svg viewBox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
+                        <circle class="b" cx="8000" cy="8000" r="5800"></circle>
+                    </svg>
+                        </div>
+                    </div>
+                    <!--Bullet Navigator End -->
+
+                </div>
+            </div>
+  
     <div id="rodape">
             <div class="rodape-box" style="width: 25%;">
                 <div class="arrow_title">
@@ -100,23 +157,65 @@
         </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
-    
-    <!-- image slider script -->
-    <script src="../js/image-slider.js"></script>
-    <script type="text/javascript">
+    <script src="../js/jquery-1.9.1.min.js"></script>
+    <script src="bootstrap.min.js"></script>
+    <script src="docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="ie10-viewport-bug-workaround.js"></script>
 
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-36251023-1']);
-        _gaq.push(['_setDomainName', 'jqueryscript.net']);
-        _gaq.push(['_trackPageview']);
+    <!-- jssor slider scripts-->
+    <script type="text/javascript" src="../js/jssor.slider.min.js"></script>
+    <script>
+        jQuery(document).ready(function($) {
 
-        (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
+            var options = {
+                $FillMode: 2, //[Optional] The way to fill image in slide, 0 stretch, 1 contain (keep aspect ratio and put all inside slide), 2 cover (keep aspect ratio and cover whole slide), 4 actual size, 5 contain for large image, actual size for small image, default value is 0
+                $AutoPlay: 1, //[Optional] Auto play or not, to enable slideshow, this option must be set to greater than 0. Default value is 0. 0: no auto play, 1: continuously, 2: stop at last slide, 4: stop on click, 8: stop on user navigation (by arrow/bullet/thumbnail/drag/arrow key navigation)
+                $Idle: 4000, //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+                $PauseOnHover: 1, //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, 4 freeze for desktop, 8 freeze for touch device, 12 freeze for desktop and touch device, default value is 1
 
+                $ArrowKeyNavigation: 1, //[Optional] Steps to go for each navigation request by pressing arrow key, default value is 1.
+                $SlideEasing: $Jease$.$OutQuint, //[Optional] Specifies easing for right to left animation, default value is $Jease$.$OutQuad
+                $SlideDuration: 800, //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
+                $MinDragOffsetToSlide: 20, //[Optional] Minimum drag offset to trigger slide, default value is 20
+                //$SlideWidth: 600,                                 //[Optional] Width of every slide in pixels, default value is width of 'slides' container
+                //$SlideHeight: 300,                                //[Optional] Height of every slide in pixels, default value is height of 'slides' container
+                $SlideSpacing: 0, //[Optional] Space between each slide in pixels, default value is 0
+                $UISearchMode: 1, //[Optional] The way (0 parellel, 1 recursive, default value is 1) to search UI components (slides container, loading screen, navigator container, arrow navigator container, thumbnail navigator container etc).
+                $PlayOrientation: 1, //[Optional] Orientation to play slide (for auto play, navigation), 1 horizental, 2 vertical, 5 horizental reverse, 6 vertical reverse, default value is 1
+                $DragOrientation: 1, //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $Cols is greater than 1, or parking position is not 0)
+
+                $BulletNavigatorOptions: { //[Optional] Options to specify and enable navigator or not
+                    $Class: $JssorBulletNavigator$, //[Required] Class to create navigator instance
+                    $ChanceToShow: 2, //[Required] 0 Never, 1 Mouse Over, 2 Always
+                    $SpacingX: 8, //[Optional] Horizontal space between each item in pixel, default value is 0
+                    $Orientation: 1 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
+                },
+
+                $ArrowNavigatorOptions: { //[Optional] Options to specify and enable arrow navigator or not
+                    $Class: $JssorArrowNavigator$, //[Requried] Class to create arrow navigator instance
+                    $ChanceToShow: 2 //[Optional] Steps to go for each navigation request, default value is 1
+                }
+            };
+
+            var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizing
+            function ScaleSlider() {
+                var bodyWidth = document.body.clientWidth;
+                if (bodyWidth)
+                    jssor_slider1.$ScaleWidth(Math.min(bodyWidth, 1920));
+                else
+                    window.setTimeout(ScaleSlider, 30);
+            }
+            ScaleSlider();
+
+            $(window).bind("load", ScaleSlider);
+            $(window).bind("resize", ScaleSlider);
+            $(window).bind("orientationchange", ScaleSlider);
+            //responsive code end
+        });
     </script>
 </body>
 
